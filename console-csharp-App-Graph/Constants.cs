32e19a082d76526c88844893dd38db90899e9d64
agrabhi <abhi.iitr@gmail.com>
2018-12-18 22:14:36 -0800

    Apps

--- console-csharp-App-Graph/Constants.cs
+++ console-csharp-App-Graph/Constants.cs
@@ -6,9 +6,10 @@
         //       see README.md for instructions
 
         // Client ID is the application guid used uniquely identify itself to the v2.0 authentication endpoint
-        public const string ClientIdForUserAuthn = "855a249a-9118-46f8-8c60-343204baf471";
+        public const string ClientIdForUserAuthn = "01b81dbd-dd72-4dcd-8171-be08345aa9b6";
+
         // Your tenant Name, for example "myb2ctenant.onmicrosoft.com"
-        public const string Tenant = "b2cconsumer.onmicrosoft.com";
+        public const string Tenant = "cpimtestabhiagr.onmicrosoft.com";
 
         // leave these as-is - URIs used for auth
         public const string AuthorityUri = "https://login.microsoftonline.com/" + Tenant + "/oauth2/v2.0/token";
@@ -18,8 +19,6 @@
         public const string AppsUri = "https://graph.microsoft.com/beta/applications";
         public const string PatchAppsUri = "https://graph.microsoft.com/beta/applications/{0}";
         public const string SPUri = "https://graph.microsoft.com/beta/serviceprincipals";
-        public const string OAuthPermissionGrantsUri = "https://graph.microsoft.com/beta/oauth2PermissionGrants";
-        public const string TrustFrameworkPolicesUri = "https://graph.microsoft.com/testcpimtf/trustFrameworkPolicies";
-        public const string TrustFrameworkPolicyByIDUri = "https://graph.microsoft.com/testcpimtf/trustFrameworkPolicies/{0}/$value";
+        public const string OAuthPermissionGrantsUri = "https://graph.microsoft.com/beta/oauth2PermissionGrants";        
     }
 }
